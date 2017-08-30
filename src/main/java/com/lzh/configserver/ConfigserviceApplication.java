e26cc57db082845c4db94c91ed4417060e485da3
scup08 <519370424@qq.com>
2017-08-30 16:39:40 +0800

    初始化

---
+++ src/main/java/com/lzh/configserver/ConfigserviceApplication.java
@@ -0,0 +1,16 @@
+package com.lzh.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserviceApplication.class, args);
+    }
+}
