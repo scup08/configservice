338f55619ff4140694087091ca82dda7fedcdc57
scup08 <519370424@qq.com>
2017-10-09 15:36:15 +0800

    a

--- src/main/java/com/lzh/configserver/ConfigserviceApplication.java
+++ src/main/java/com/lzh/configserver/ConfigserviceApplication.java
@@ -3,9 +3,11 @@ package com.lzh.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigserviceApplication {
 
     public static void main(String[] args) {
